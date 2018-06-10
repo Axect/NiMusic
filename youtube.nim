0d2b8dade72eba3f0ceff0b0c59de75fa37cb2a3
Axect <edeftg@gmail.com>
2018-06-11 01:50:29 +0900

    Successfully Download & Convert

--- youtube.nim
+++ youtube.nim
@@ -3,32 +3,38 @@ import os, osproc, strutils, strformat, sequtils, re
 let callYoutube: string = "youtube-dl --embed-thumbnail --format mp4 "
 proc convertMp3(name: string, target: string): string = &"ffmpeg -i {name} -b:a 320K -vn {target}"
 
-let slugify = [("(", "\\("),(")", "\\)"), (" ", ""), ("./", ""), ("&", "_")]
+let weakSlugify = [(" ", ""), ("./", ""), ("&", "_")]
+let strongSlugify = [("(", "\\("),(")", "\\)")]
 
 proc main =
   let x = open("link.csv")
   # Downloading
+  var candi: seq[string] = @[]
   for link in x.lines:
-    let output = execProcess(callYoutube & link.string)
+    candi.add(callYoutube & link.string)
     echo(&"Downloading {link}...")
-    echo output
+  let err1 = execProcesses(candi)
+  echo err1
 
   # Slugify
-  var vids: seq[string] = @[]
-  var mp3s: seq[string] = @[]
+  var candi2: seq[string] = @[]
   for file in walkDirRec "./":
     if file.match re".*\.mp4":
-      let revid = multiReplace(file, slugify)
+      let revid = multiReplace(file, weakSlugify)
       let remp3 = multiReplace(revid,[(".mp4", ".mp3")])
-      vids.add(revid)
-      mp3s.add(remp3)
+      
       moveFile(file, revid)
+      
+      let revid2 = multiReplace(revid, strongSlugify)
+      let remp32 = multiReplace(remp3, strongSlugify)
+      let temp1 = convertMp3(revid2, remp32)
+      
+      candi2.add(temp1)
+
       echo "Slugify Finish!"
-  
-  for _, names in zip(vids, mp3s):
-    let temp = convertMp3(names.a, names.b)
-    let output2 = execProcess(temp)
-    echo output2
+
+  let err2 = execProcesses(candi2)
+  echo err2
 
   echo "All Done!"
 
