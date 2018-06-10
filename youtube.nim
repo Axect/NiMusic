64ce73a44b48264e6292a424c145a8cc1eb6d504
Axect <edeftg@gmail.com>
2018-06-11 01:21:09 +0900

    Real Practice 1

--- youtube.nim
+++ youtube.nim
@@ -1,34 +1,37 @@
-import os, osproc, strutils, strformat, sequtils
+import os, osproc, strutils, strformat, sequtils, re
 
-let callYoutube: string ="youtube-dl --embed-thumbnail --format mp4 "
+let callYoutube: string = "youtube-dl --embed-thumbnail --format mp4 "
+proc convertMp3(name: string, target: string): string = &"ffmpeg -i {name} -b:a 320K -vn {target}"
 
-
-#var nim = &"./Nim/bin/p00{num}"
-
-#case num
-#of 1..9: nim = &"./Nim/bin/p00{num}"
-#of 10..100: nim = &"./Nim/bin/p0{num}"
-#else: nim = &"./Nim/bin/p{num}"
+let slugify = [("(", "\\("),(")", "\\)"), (" ", ""), ("./", ""), ("&", "_")]
 
 proc main =
   let x = open("link.csv")
-  var vids: seq[string] = @[]
+  # Downloading
   for link in x.lines:
     let output = execProcess(callYoutube & link.string)
     echo(&"Downloading {link}...")
     echo output
-    vids.add(multiReplace(link,[("(", "\\("),(")", "\\)"), (" ", "")]))
-  for vid in vids:
-    let output2 = 
+
+  # Slugify
+  var vids: seq[string] = @[]
+  var mp3s: seq[string] = @[]
+  for file in walkDirRec "./":
+    if file.match re".*\.mp4":
+      let revid = multiReplace(file, slugify)
+      let remp3 = multiReplace(revid,[(".mp4", ".mp3")])
+      vids.add(revid)
+      mp3s.add(remp3)
+      moveFile(file, revid)
+      echo "Slugify Finish!"
+  
+  for _, names in zip(vids, mp3s):
+    let temp = convertMp3(names.a, names.b)
+    let output2 = execProcess(temp)
+    echo output2
+
+  echo "All Done!"
 
 
-#proc main =
-#  let output = case num
-#  of 1..9: execProcess(&"hyperfine -w 3 --export-markdown Bench/euler00{num}.md " & rust & " " & nim & " " & haskell & python)
-#  of 10..99: execProcess(&"hyperfine -w 3 --export-markdown Bench/euler0{num}.md " & rust & " " & nim & " " & haskell & python)
-#  else: execProcess(&"hyperfine -w 3 --export-markdown Bench/euler{num}.md " & rust & " " & nim & " " & haskell & python)
-#
-#  echo output
-#
 if isMainModule:
   main()
